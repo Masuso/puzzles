d808dd1d824eeb73e28294a59ca37b6a05dd1333
Masuso <99345946+Masuso@users.noreply.github.com>
2022-06-27 10:56:09 +0200

    Added puzzle of converting integer to roman numeral

--- roman numerals.py
+++ roman numerals.py
@@ -14,3 +14,21 @@ def romanToInt(self, s: str) -> int:
             total = total + roman[s[i]]
     return total
 
+# Puzzle:
+# Given an integer, convert it to a roman numeral.
+
+def intToRoman(self, num: int) -> str:
+    # 59 ms, 13.8 mb, o(n)
+    # learned some neat symbols like what // and % does
+    symbols = [[1, 'I'], [4, 'IV'], [5, 'V'], [9, 'IX'],
+               [10, 'X'], [40, 'XL'], [50, 'L'], [90, 'XC'],
+               [100, 'C'], [400, 'CD'], [500, 'D'], [900, 'CM'],
+               [1000, 'M']]
+    roman_numeral = ''
+    for val, sym in reversed(symbols):
+        if num // val:
+            roman_numeral += sym * (num // val)
+            num = num % val
+    return roman_numeral
+
+
