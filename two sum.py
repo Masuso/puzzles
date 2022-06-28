8f8418431c5ed2235bc2f8560f9e96c9d34634b3
Masuso <99345946+Masuso@users.noreply.github.com>
2022-06-28 17:05:01 +0200

    Added solution for puzzle of finding two numbers that sum to target in sorted list.

--- two sum.py
+++ two sum.py
@@ -21,3 +21,21 @@ def twoSum_hashmap(self, nums: List[int], target: int) -> List[int]:
         if sum1 in checked:
             return [i, checked[sum1]]
         checked[nums[i]] = i
+
+# Puzzle:
+# Given a 1-indexed array of integers numbers that is already sorted in
+# non-decreasing order, find two numbers such that they add up to a specific target number.
+
+def twoSum_sorted(self, numbers: List[int], target: int) -> List[int]:
+    # 243 ms, 14.9 mb, o(n)
+    # got an error for using two if loops instead of if and elif
+    l, r = 0, len(numbers) - 1
+    while l < r:
+        current_sum = numbers[l] + numbers[r]
+        if current_sum < target:
+            l += 1
+        elif current_sum > target:
+            r -= 1
+        else:
+            return [l + 1, r + 1]
+
