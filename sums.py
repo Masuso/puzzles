0fdb670ef012c92c5f3aa477c265b1363154f8b6
Masuso <99345946+Masuso@users.noreply.github.com>
2022-06-29 18:00:00 +0200

    Added 3 sum puzzle and changed file name.

--- sums.py
+++ sums.py
@@ -39,3 +39,29 @@ def twoSum_sorted(self, numbers: List[int], target: int) -> List[int]:
         else:
             return [l + 1, r + 1]
 
+# Puzzle:
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
+# such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+def threeSum(self, nums: List[int]) -> List[List[int]]:
+    # 18.2 mb, O(nlogn) + O(n^2) = O(n^2)
+    # lots of trouble getting this one to work. I got reminded of how to use continue
+    # important to remember to increment the pointers when we find a set so as not get duplicates.
+    sets = []
+    nums.sort()
+    for i in range(0, len(nums)):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+            threesum = nums[i] + nums[l] + nums[r]
+            if threesum < 0:
+                l += 1
+            elif threesum > 0:
+                r -= 1
+            else:
+                sets.append([nums[i], nums[l], nums[r]])
+                l += 1
+                while nums[l] == nums[l - 1] and l < r:
+                    l += 1
+    return sets
